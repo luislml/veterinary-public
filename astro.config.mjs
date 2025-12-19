93e2c5700b8b513fc5409fee038860c0f184999a
Luis Soto <s.r.l.a10539595@gmail.com>
2025-12-18 23:15:29 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -12,5 +14,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
